13bb380599bc46b0eaadf04b81d59d7f009b82c0
hhtboy <hhtboy1024@gmail.com>
2023-12-10 19:06:03 +0900

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import requests
+from selenium import webdriver
+import json
+from bs4 import BeautifulSoup
+from urllib.parse import urlencode, urljoin
+import time
+
+
+proxies = {"http": "socks5h://localhost:9050", "https": "socks5h://localhost:9050"}
+
+# 전역 변수와 공톰 함수 입니다.
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "ko-KR,ko;q=0.9",
+    "Cache-Control": "max-age=0",
+    "Sec-Ch-Ua": '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"macOS"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+}
+forum = []
+errorForum = []
+
+
+def userStatus(Bool, name):
+    if Bool:
+        forum.append(name)
+
+
+def notSearch(name):
+    errorForum.append(name)
+
+
+# 여기서 부터 작성하세요
+
+
+if __name__ == "__main__":
+    user = input("유저명 : ")
+
+    print(forum)
+    print(errorForum)
