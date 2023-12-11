870044fb13ce46e84010e5e0fe8819fc9d19876d
XH <87056811+xiihyun@users.noreply.github.com>
2023-12-11 11:01:39 +0000

    hostingforums, landzdown, wilderssecurity

--- main.py
+++ main.py
@@ -9,4 +9,37 @@ def hostingforums(user):
     except :
         notSearch(name)
 
+def landzdown(user):
+    url = "https://www.landzdown.com/index.php?action=mlist;sa=search"
+    name = 'landzdown'
+    payload = {
+        'search': user,
+        'fields[]': 'name',
+        'submit': 'Search'
+    }
+    
+    try:
+        response = requests.post(url, headers=headers, data=payload)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            rows = soup.find_all('td', class_='real_name lefttext')
+            found = any(row.get_text(strip=True) == user for row in rows)
+            userStatus(found, name)
+    except Exception as e:
+        print(e)
+        notSearch(name)
 
+def wilderssecurity(user):
+    url = "https://www.wilderssecurity.com/search/search"
+    name = 'wilderssecurity'
+    payload = {
+        "keywords": "",
+        "users": user,
+        "date": "",
+        "_xfToken": ""}
+    try :
+        response = requests.post(url, headers=headers, data=payload)
+        if 'The following members could not be found' not in response.text:
+            userStatus(True, name)
+    except :
+        notSearch(name)
