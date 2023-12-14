1775fe4c67e23999beba5e6c8e2b3b2dda8115ab
XH <87056811+xiihyun@users.noreply.github.com>
2023-12-14 13:20:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,37 @@ print(f.renderText('Whats My Name'))
 print(colored.white("----------------------------------------------------------KILLER WHALE _whs"))
 
 
+proxies = {"http": "socks5h://localhost:9050", "https": "socks5h://localhost:9050"}
+
+# 전역 변수와 공톰 함수 입니다.
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "ko-KR,ko;q=0.9",
+    "Cache-Control": "max-age=0",
+    "Sec-Ch-Ua": '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"macOS"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+}
+forum = []
+errorForum = []
+
+
+def userStatus(Bool, name):
+    if Bool:
+        forum.append(name)
+
+
+def notSearch(name):
+    errorForum.append(name)
+
+
 
 # 여기서 부터 작성하세요
 def _0Day(user):
