abe64872cb476a645fd7c0fdd9e536a33f2a2fe4
Ha3To <lgsc0409ld@gmail.com>
2023-12-26 02:21:55 +0900

    dark2web uid출력버전

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import requests
-from selenium import webdriver
-import json
 from bs4 import BeautifulSoup
-from urllib.parse import urlencode, urljoin
-import time
 
+# UI 관련 내용입니다.
+from pyfiglet import Figlet
+from clint.textui import colored
 
-proxies = {"http": "socks5h://localhost:9050", "https": "socks5h://localhost:9050"}
+f = Figlet(font="slant")
+print(
+    colored.white(
+        "---------------------------------------------------------------------------"
+    )
+)
+print(f.renderText("Whats My Name"))
+print(
+    colored.white(
+        "----------------------------------------------------------KILLER WHALE _whs"
+    )
+)
 
-# 전역 변수와 공톰 함수 입니다.
+proxies = {
+    'http': 'socks5h://localhost:9050',
+    'https': 'socks5h://localhost:9050'
+}
+
+# 전역 변수와 공통 함수 입니다.
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "Accept-Encoding": "gzip, deflate, br",
@@ -27,57 +42,43 @@ headers = {
 forum = []
 errorForum = []
 
-
 def userStatus(Bool, name):
     if Bool:
         forum.append(name)
 
-
 def notSearch(name):
     errorForum.append(name)
 
-
-# 여기서 부터 작성하세요
-def wasm(user):
-    url = "https://wasm.in/index.php?members/find"
-    name = "Wasm.in"
-    payload = {
-        "q": user,
-        "_xfRequestUri": "/",
-        "_xfNoRedirect": 1,
-        "_xfResponseType": "json"
-    }
+def dark2web(user):
+    url = "https://web-1.gate2dark.online"
+    name = 'dark2web'
     try:
-        response = requests.post(url, data=payload, headers=headers)
-
-        if response.status_code == 200 and user in response.json().get("results", {}):
-            userStatus(True, name)
-
+        response = requests.get(url)
+        cookies = response.cookies
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        element = soup.find(id='XF')
+        data_csrf = element['data-csrf']
+        url = 'https://web-1.gate2dark.online/index.php?members/find&_xfRequestUri=%2Fmembers%2F&_xfWithData=1&_xfResponseType=json&_xfToken='+data_csrf+'&q='+user
+        response = requests.post(url, cookies=cookies)
+        json_data = response.json()
+        for item in json_data['results']:
+            id_value = item['id']
+            if id_value.lower() == user.lower():
+                userID = item['iconHtml'].split('data-user-id="')[1].split('"')[0]
+                userStatus(True, f"{name}(userID: {userID})")
     except Exception as e:
         notSearch(name)
-
-def redSecurity(user):
-    url = "https://redsecurity.info/cc/xmlhttp.php?action=get_users"
-    name = "RedSecurity"
-    timestamp = int(time.time() * 1000)
-    payload = {
-        "query": user,
-        "_": timestamp
-    }
-    try:
-        response = requests.post(url, data=payload, headers=headers)
-        if response.status_code == 200:
-            result = response.json()
-            if result and user == result[0].get("id"):
-                userStatus(True, name)
-
-    except Exception as e:
-        notSearch(name)
-
 
 if __name__ == "__main__":
     user = input("유저명 : ")
-    wasm(user)
-    redSecurity(user)
-    print(forum)
-    print(errorForum)
+    dark2web(user)
+
+    print(colored.green("\n>>> DETECTED: "))
+    for i in forum:
+        print("\t" + i)
+
+    print(colored.red("\n>>> unknown: ", errorForum))
+    for i in errorForum:
+        print("\t" + i)
+
