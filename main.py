f71e3868c90b4592366d58423dc2b369f753e726
Ha3To <lgsc0409ld@gmail.com>
2023-12-27 13:38:39 +0900

    dark2web uid conflict 수정

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ headers = {
     "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
 }
+
 forum = []
 errorForum = []
 
